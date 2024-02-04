72e2a6ae73c41a2f86d3d5caa45d7722710e853b
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-03 18:12:34 -0600

    Initial Commit

---
+++ TacticalExpense/TacticalExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacticalExpenseApp.swift
+//  TacticalExpense
+//
+//  Created by J. DeWeese on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TacticalExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
